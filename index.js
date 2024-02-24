02252ca11fd2a4076395e0a27b1e389df675beaa
MiDimas <103483149+MiDimas@users.noreply.github.com>
2024-02-24 23:26:30 +0800

    убрана искуственная задержка ответа

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const router = jsonServer.router(path.resolve(__dirname, 'db.json'));
 server.use(jsonServer.defaults({}));
 server.use(jsonServer.bodyParser);
 
-server.use(async (req, res, next) => {
-    await new Promise((res) => { setTimeout(res, 800); });
-    next();
-});
 
 server.post('/login', (req, res) => {
     try {
